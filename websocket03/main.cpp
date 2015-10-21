cee156345f487d850c7e8ad56679b21ec1a2d63a
Claus Guttesen <kometen@gmail.com>
2015-10-21 09:56:22 +0200

    Initial Commit

---
+++ websocket03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  websocket03
+//
+//  Created by Claus Guttesen on 21/10/2015.
+//  Copyright © 2015 Claus Guttesen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
